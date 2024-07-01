d17eec3dd65f6a351029e7572469693d0a4f8d01
Cyano Hao <c@cyano.cn>
2024-07-01 10:38:13 +0800

    Support for The War Within

--- GreenWall.lua
+++ GreenWall.lua
@@ -187,7 +187,8 @@ function GreenWall_OnLoad(self)
     self.default = function(self)
         GreenWallInterfaceFrame_SetDefaults(self)
     end
-    InterfaceOptions_AddCategory(self)
+    local category = Settings.RegisterCanvasLayoutCategory(self, "GreenWall")
+    Settings.RegisterAddOnCategory(category)
 end
 
 
